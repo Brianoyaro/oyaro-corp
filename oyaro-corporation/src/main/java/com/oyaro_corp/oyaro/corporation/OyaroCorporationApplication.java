7edc890b3d3359ff2702489bc8ab2014f7fbfcbb
Brianoyaro <brianoyaro2000@yahoo.com>
2026-04-04 19:31:25 +0300

    blueprint of the application

---
+++ oyaro-corporation/src/main/java/com/oyaro_corp/oyaro/corporation/OyaroCorporationApplication.java
@@ -0,0 +1,13 @@
+package com.oyaro_corp.oyaro.corporation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OyaroCorporationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OyaroCorporationApplication.class, args);
+	}
+
+}
---
+++ oyaro-corporation/src/test/java/com/oyaro_corp/oyaro/corporation/OyaroCorporationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oyaro_corp.oyaro.corporation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OyaroCorporationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
