75272056051562ca15f755f4a34dac398bec53bd
Brianoyaro <brianoyaro2000@yahoo.com>
2026-04-05 19:24:25 +0300

    finished implementing JwtFilter

--- oyaro-corporation/src/main/java/com/oyaro_corp/oyaro/corporation/config/JwtFilter.java
+++ oyaro-corporation/src/main/java/com/oyaro_corp/oyaro/corporation/config/JwtFilter.java
@@ -1,16 +1,36 @@
 package com.oyaro_corp.oyaro.corporation.config;
 
+import com.oyaro_corp.oyaro.corporation.service.CustomUserDetailsService;
+import com.oyaro_corp.oyaro.corporation.service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.jspecify.annotations.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    public JwtFilter(JwtService jwtService, CustomUserDetailsService customUserDetailsService) {
+        this.jwtService = jwtService;
+        this.customUserDetailsService = customUserDetailsService;
+    }
+
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
+
         // Skip JWT authentication for endpoints that are public
         final String requestPath = request.getServletPath();
         if (requestPath.startsWith("/api/auth/")) {
@@ -20,12 +40,48 @@ public class JwtFilter extends OncePerRequestFilter {
 
         // Extract the token
         final String authHeader = request.getHeader("Authorization");
+
         if (authHeader == null && !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        String token = authHeader.substring(7);
+        final String token = authHeader.substring(7);
+        final String userEmail;
         // work on jwtService then continue with the jwtFilter logic
+        try {
+            // extract username from jwt
+            userEmail = jwtService.extractUsername(token);
+
+            if (userEmail == null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = customUserDetailsService.loadUserByUsername(userEmail);
+
+                if (jwtService.isTokenValid(token, userDetails)) {
+                    // validate the audience and issuer to prevent JWT tampering
+                    if (jwtService.validateAudience(token) && jwtService.validateIssuer(token)) {
+                        // create authentication token
+                        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                                userDetails,
+                                null,
+                                userDetails.getAuthorities()
+                        );
+
+                        // set additional details
+                        authentication.setDetails(
+                                new WebAuthenticationDetailsSource().buildDetails(request)
+                        );
+
+                        // set authentication in the security context
+                        SecurityContextHolder.getContext().setAuthentication(authentication);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            logger.error("JWT authentication exception", e);
+//            e.printStackTrace();
+        }
+
+        // continue with the next filter chain
+        filterChain.doFilter(request, response);
     }
 }
