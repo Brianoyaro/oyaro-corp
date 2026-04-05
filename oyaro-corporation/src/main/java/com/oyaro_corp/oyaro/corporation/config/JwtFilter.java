3c42857907f82449cb0a36d6f4ce6fa04f2fac8a
Brianoyaro <brianoyaro2000@yahoo.com>
2026-04-05 18:24:23 +0300

    implemented some core JwtFilter. The next implementation is dependent on jwtService which is currently unimplemented

--- oyaro-corporation/src/main/java/com/oyaro_corp/oyaro/corporation/config/JwtFilter.java
+++ oyaro-corporation/src/main/java/com/oyaro_corp/oyaro/corporation/config/JwtFilter.java
@@ -1,6 +1,31 @@
 package com.oyaro_corp.oyaro.corporation.config;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-public class JwtFilter implements OncePerRequestFilter {
+import java.io.IOException;
+
+public class JwtFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // Skip JWT authentication for endpoints that are public
+        final String requestPath = request.getServletPath();
+        if (requestPath.startsWith("/api/auth/")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // Extract the token
+        final String authHeader = request.getHeader("Authorization");
+        if (authHeader == null && !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authHeader.substring(7);
+        // work on jwtService then continue with the jwtFilter logic
+    }
 }
