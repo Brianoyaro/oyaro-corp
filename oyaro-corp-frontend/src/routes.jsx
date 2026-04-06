fee63c0c758229395cdbc8866b137627acc5cf49
Brianoyaro <brianoyaro2000@yahoo.com>
2026-04-06 14:20:35 +0300

    added frontend structure

---
+++ oyaro-corp-frontend/src/routes.jsx
@@ -0,0 +1,37 @@
+import { createBrowserRouter } from "react-router-dom";
+
+// main app component
+import App from './App.jsx'
+
+// authentication pages
+import Login from './pages/authentication/Login.jsx'
+import Register from './pages/authentication/Register.jsx'
+
+// error pages
+import NotFound from './pages/errors/NotFound.jsx'
+
+const router = createBrowserRouter([
+  // Home Route
+  {
+    path: "/",
+    element: <App />,
+  },
+  // Login Route
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  // Register Route
+  {
+    path: "/register",
+    element: <Register />,
+  },
+
+  // 404 Not Found Route
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+export default router;
