d34de0509cc2cb4f0f4ee2a1a585a9a410ad7f45
Brianoyaro <brianoyaro2000@yahoo.com>
2026-04-06 18:20:42 +0300

    added tailwindcss

--- oyaro-corp-frontend/src/pages/authentication/Login.jsx
+++ oyaro-corp-frontend/src/pages/authentication/Login.jsx
@@ -4,48 +4,9 @@ import { useQuery } from '@tanstack/react-query'
 
 
 function Login() {
-  
-  // use tanStack query to fetch login data and manage loading and error states
-  const { data, error, isPending, refetch } = useQuery({
-    queryKey: ['loginData'],
-    queryFn: () => authService.login({ email: 'test1@email.com', password: 'passwordd' }),
-    retry: false,
-  });
-
-  /*const [responseData, setResponseData] = useState(null);
-
-  useEffect(() => {
-  const fetchData = async () => {
-    try {
-      const response = await authService.login({ email: 'test1@email.com', password: 'password' });
-      setResponseData(response.data);
-    } catch (error) {
-      console.error('Login failed:', error);
-    }
-  };
-  fetchData();
-}, []); // Empty array runs effect only on mount*/
-
-  if (isPending) return <span>Loading...</span>
-
-    if (error)
-    return (
-      <span>
-        Error: {error.message} <button onClick={refetch}>retry</button>
-      </span>
-    )
-
   return (
-    <div>
-      <h1>Login Page</h1>
-      {data && (
-        <div>
-          <h2>Login Response:</h2>
-          <pre>{JSON.stringify(data, null, 2)}</pre>
-        </div>
-      )}
-    </div>
-  )
+    <h1 className="bg-blue-500 text-white p-4">Login Page</h1>
+  );
 }
 
 export default Login
