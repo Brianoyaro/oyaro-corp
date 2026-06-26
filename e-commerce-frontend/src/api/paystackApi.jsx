b6f4adb0b770e420d465b8680621995947783a0d
Brianoyaro <brianoyaro2000@yahoo.com>
2026-06-26 18:26:25 +0300

    added paystack frontend api

---
+++ e-commerce-frontend/src/api/paystackApi.jsx
@@ -0,0 +1,29 @@
+import apiClient from "./apiClient";
+
+export const paystackApi = {
+    initialize: async (data) => {
+        try {
+            const response = await apiClient.post('/paystack/initialize', data)
+            return response.data
+        } catch (error) {
+        const errorMessage =
+            error.response?.data?.message ||
+            error.message ||
+            'Failed to fetch all products';
+            throw new Error(errorMessage);
+        }
+    },
+
+    verify: async (receipt) => {
+        try {
+            const response = await apiClient.post(`/paystack/${receipt}`)
+            return response.data
+        } catch (error) {
+        const errorMessage =
+            error.response?.data?.message ||
+            error.message ||
+            'Failed to fetch all products';
+            throw new Error(errorMessage);
+        }
+    }
+}
